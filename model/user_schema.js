8438cafa9e1c2a755537b0e1535611f0825fcc8a
mehuldantani97@gmail.com <mehuldantani97@gmail.com>
2023-04-19 09:06:05 +0530

    created userschema methods

--- model/user_schema.js
+++ model/user_schema.js
@@ -1,5 +1,9 @@
 import mongoose from "mongoose";
 import AuthRoles from '../utils/authroles'
+import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
+import crypto from "crypto"     //This  is default package comes with Nodejs.
+import config from "../config/index"
 
 //create a userschema for userinfomation
 const userSchema = mongoose.Schema(
@@ -30,5 +34,37 @@ const userSchema = mongoose.Schema(
     },
     { timestamps: true }            //keep track of timestamps
 );
+
+//encrypt the password before saving it the DB.
+userSchema.pre("save",async function(next){
+    
+    //if password is not modified then return next()
+    if(!this.modified("password")) return next()
+
+    //get the encrypted password
+    this.password = await bcrypt.hash(this.password,10);
+
+    next()
+})
+
+//add more features to schema
+userSchema.methods = {
+    //compare password
+    comparePasssword: async function(enteredPassword){
+        return await bcrypt.compare(enteredPassword,this.password);
+    },
+
+    //generate JWT(Json Web Token)
+    getJwtToken: function(){
+        return jwt.sign({
+            _id: this._id,
+            role: this.role
+        },
+        config.JWT_SECRET,
+        {
+            expiresIn: config.JWT_EXPIRY
+        })
+    }
+}
 
 export default mongoose.model("User",userSchema);
