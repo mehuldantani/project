69bd7056e39e09429d25a47e1d1075feb83f1146
mehuldantani97@gmail.com <mehuldantani97@gmail.com>
2023-04-20 23:16:39 +0530

    server start app with db connect

--- config/index.js
+++ config/index.js
@@ -6,6 +6,8 @@ dotenv.config()
 const config = {
     JWT_SECRET: process.env.JWT_SECRET,
     JWT_EXPIRY: process.env.JWT_EXPIRY || "30d",
+    MONGODB_URL:process.env.MONGODB_URL,
+    PORT: process.env.PORT || 4000
 }
 
 export default config
---
+++ index.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config/index";
+
+//self/Immediately Invoked Function Expression (IIFY)
+//create a function and execute immediately
+(async () => {
+    try {
+        await mongoose.connect(config.MONGODB_URL)
+        console.log("DB Connected Successfully")
+
+        app.on('error',(err) => {
+            console.log("Error",err);
+            throw err
+        })
+
+        //calllback function
+        const onListening = () =>{
+            console.log(`Server started on ${config.PORT}.`)
+        }
+
+        //start app
+        app.listen(config.PORT, onListening)
+
+    } catch (err) {
+        console.log("ERROR",err);
+        throw err
+    }
+
+})()
