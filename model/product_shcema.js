4a4f4da17471d84cb8bd77f09033f782bf0b185e
mehuldantani97@gmail.com <mehuldantani97@gmail.com>
2023-04-20 09:05:16 +0530

    updated prodct schema

--- model/product_shcema.js
+++ model/product_shcema.js
@@ -1,7 +1,42 @@
 import mongoose from "mongoose";
 
-const productSchema = mongoose.Schema({
+const productSchema = mongoose.Schema(
+    {
     name:{
         type: String,
+        required:[true,'Product must have a name.'],
+        trim: true,
+        maxlength:[120,'Product name must be within 120 chars.']
+    },
+    price:{
+        type:Number,
+        reqired:[true,"Provide a product number."],
+        maxlength:[5,'You can\'t seel so much expensive product here.']
+    },
+    description:{
+        type: String,
+        //use some form of editor - personal assigment.
+    },
+    photos:[{
+        secure_url:{
+            type:String,
+            required:true
+        }
+    }],
+    stock:{
+        type:Number,
+        default: 0
+    },
+    soldUnits:{
+        type:Number,
+        default:0
+    },
+    collectionId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"collection"
     }
-})
+    },
+    {timestamps: true}
+)
+
+export default mongoose.model('Product',productSchema)
