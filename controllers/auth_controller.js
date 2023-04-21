9dfc18ed1a5899e8e3f44e11c9ce28d77c17bdbb
mehuldantani97@gmail.com <mehuldantani97@gmail.com>
2023-04-21 23:09:53 +0530

    forgot password change

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -1,6 +1,8 @@
+import user_schema from "../model/user_schema.js";
 import User from "../model/user_schema.js"
 import asyncHandler from '../services/async_handler.js'
 import customerror from '../utils/custom_error.js'
+import email from "../utils/email.js"
 
 export const cookieOptions = {
     expires: new Date(Date.now() + 3*24*60*60*1000),
@@ -122,4 +124,65 @@ export const logout = asyncHandler(async (_req,res) =>{
         success:true,
         message: "Logged Out"
     })
+})
+
+/******************************************************
+ * @FORGOT_PASSWORD
+ * @route http://localhost:4000/api/auth/password/forgot
+ * @description user will submit email and we will generate a token and send a email with link
+ * @parameters  email
+ * @returns Success message - email send to your email
+ ********************************************************/
+
+export const forgotPassword = asyncHandler(async(req,res)=>{
+
+    const {email} = req.body
+
+    //validation
+    if (!email){
+        throw new customerror('Please fill all the fields',400)
+    }
+
+    //check if user exists
+    const existingUser = User.findOne({email})
+
+    //if user not found then return
+    if(!existingUser){
+        throw new customerror('User Not Found',400)
+    }
+
+    //get forgot pw token
+    const resetToken = user.generateForgotPWToken()
+
+    //savve info in DB
+    await user.save({validateBeforeSave: false})
+
+    //generate reset password url
+    const resetUrl = `${req.protocol}://${req.get("host")}/api/auth/password/forgot/${resetToken}`
+
+    //email content
+    const text = `Your Password reset url is 
+    \n\n ${resetUrl}\n\n`
+
+    //send and email
+    try {
+        await email({
+            email: user.email,
+            subject: "Reset password - Mehul",
+            text:text
+        })
+
+        //send response
+        res.status(200).json({
+            success: true,
+            message: `We have sent an email to ${user.email} with Reset password link.`
+        })
+    } catch (err) {
+
+        //Rollback the chanes on email failure
+        user.forgotpasswordExpiry = undefined
+        user.forgotpasswordToken = undefined
+        
+        throw new customerror('Failed',400)
+    }
 })
