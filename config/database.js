15afdad388b47782a86597dcf387119bded9ea14
mehuldantani97@gmail.com <mehuldantani97@gmail.com>
2023-04-29 20:12:55 +0530

    server started successfully :):):)

--- config/database.js
+++ config/database.js
@@ -1,21 +1,22 @@
 const mongoose = require('mongoose');
+const config = require("./config.js");
 
-//URL to connect to mongoDB
-const MONGODB_URL = process.env.MONGODB_URL
-//export function that can be used in other files to connect the db.
+// URL to connect to mongoDB
+const MONGODB_URL = config.MONGODB_URL
+console.log(MONGODB_URL)
+// export function that can be used in other files to connect the db.
 exports.connect = () => {
-
-    //connect to db
-    mongoose.connect(MONGODB_URL,{
+    // connect to db
+    mongoose.connect(MONGODB_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true
-    }).then(()=>{
-        console.log('DB Connected Succesfully.')
+    }).then(() => {
+        console.log('DB Connected Successfully.')
     }).catch((error) => {
         console.log('DB connection failed.')
         console.log(error)
 
-        //exit with failure code 1- failure 0-close after db connect
+        // exit with failure code 1- failure 0-close after db connect
         process.exit(1)
     })
 }
