a755fe42c08540dbca2dd1f2de945cae1bcf08f9
mehuldantani97@gmail.com <mehuldantani97@gmail.com>
2023-05-14 13:29:10 +0530

    move email to production level

--- config/transporter_config.js
+++ config/transporter_config.js
@@ -2,9 +2,7 @@ const nodemailer = require("nodemailer")
 const config = require("./config")
 
 let transporter = nodemailer.createTransport({
-    host: config.SMTP_MAIL_HOST,
-    port: config.SMTP_MAIL_PORT,
-    secure: false, // upgrade later with STARTTLS
+    service: "gmail",
     auth: {
       user: config.SMTP_MAIL_UN,
       pass: config.SMTP_MAIL_PW
